bdd4bfced4b7bda5c5f32e3fc5bb6a1b438a3249
Michael Favvas <122233869+lpolpo14@users.noreply.github.com>
2026-04-25 21:43:03 +0300

    Setting up

---
+++ src/main/java/unipi/d3fender/D3fenderApplication.java
@@ -0,0 +1,13 @@
+package unipi.d3fender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class D3fenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(D3fenderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/unipi/d3fender/D3fenderApplicationTests.java
@@ -0,0 +1,13 @@
+package unipi.d3fender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class D3fenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
