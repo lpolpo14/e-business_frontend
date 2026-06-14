357723ac6ee4d1a74ee75deb988862d7f102ed76
Michael Favvas <122233869+lpolpo14@users.noreply.github.com>
2026-06-14 03:08:57 +0300

    Added default admin user to DB. Improved UI for Admin dashbaord and SBOM.

--- src/main/java/unipi/d3fender/D3fenderApplication.java
+++ src/main/java/unipi/d3fender/D3fenderApplication.java
@@ -1,7 +1,13 @@
 package unipi.d3fender;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import unipi.d3fender.models.User;
+import unipi.d3fender.repositories.UserRepository;
 
 @SpringBootApplication
 public class D3fenderApplication {
@@ -10,4 +16,30 @@ public class D3fenderApplication {
 		SpringApplication.run(D3fenderApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner initializeAdminUser(
+			UserRepository userRepository,
+			PasswordEncoder passwordEncoder,
+			@Value("${d3fender.admin.username}") String adminUsername,
+			@Value("${d3fender.admin.email}") String adminEmail,
+			@Value("${d3fender.admin.password}") String adminPassword
+	) {
+		return args -> {
+			boolean adminExists = userRepository.findByUsername(adminUsername).isPresent();
+
+			if (adminExists) {
+				return;
+			}
+
+			User admin = new User();
+			admin.setUsername(adminUsername);
+			admin.setEmail(adminEmail);
+			admin.setPassword(passwordEncoder.encode(adminPassword));
+
+
+			admin.setSubscriptionPlan("PRO");
+
+			userRepository.save(admin);
+		};
+	}
 }
